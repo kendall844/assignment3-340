5b6df7a84a0deca566f69f999d60b3832cd90bcb
Kendall Williford <KMWILLIFORD@uncg.edu>
2026-03-06 16:15:40 -0500

    Added code for controller, service, repo classes

--- src/main/java/com/example/demo/CharacterController.java
+++ src/main/java/com/example/demo/CharacterController.java
@@ -17,6 +17,36 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RestController
 @RequestMapping("/api/chacters")
 public class CharacterController {
+    
+    private final CharacterService characterService;
 
+    public CharacterController(CharacterService characterService){
+        this.characterService = characterService;
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<Collection<Character>>getAllCharacters(){
+        return ResponseEntity.ok(characterService.getAllCharacters());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Character> getCharacterById(@PathVariable Long id){
+        Character character = characterService.getCharacterById(id);
+        if(character != null){
+            return ResponseEntity.ok(character);
+        }else{
+            return ResponseEntity.notFound().build();
+        }
+        }
+
+        @PostMapping("/")
+        public ResponseEntity<Character> createCharacter(@RequestBody Character character){
+            Character createdCharacter = characterService.createdCharacter(character);
+            if (createdCharacter != null){
+                return ResponseEntity.ok(createdCharacter);
+            }else{
+                return ResponseEntity.notFound().build();
+            }
+        }
     }
 
--- src/main/java/com/example/demo/CharacterService.java
+++ src/main/java/com/example/demo/CharacterService.java
@@ -12,4 +12,44 @@ public class CharacterService {
     public CharacterService(CharacterRepository characterRepository){
         this.characterRepository = characterRepository;
     }
+
+    public List<Character> getAllCharacters(){
+        return characterRepository.findAll();
+    }
+
+    public Character createCharacter(Character character){
+        return characterRepository.save(character);
+    }
+
+    public Character getCharacterById(Long id){
+        return characterRepository.findById(id).orElse(null);
+    }
+
+    public Character updateCharacter(Long id, Character updatedCharacter){
+        return characterRepository.findById(id)
+        .map(character -> {
+            character.setName(updatedCharacter.getName());
+            character.setDescription(updatedCharacter.getDescription());
+            character.setUniverse(updatedCharacter.getUniverse());
+            character.setAge(updatedCharacter.getAge());
+            return characterRepository.save(character);
+        })
+        .orElse(null);
+    }
+
+    public void deleteCharacter(Long id){
+        characterRepository.deleteById(id);
+    }
+
+    public List<Character> getOlderCharacters(double age){
+        return characterRepository.findOlderCharacters(age);
+    }
+
+    public List<Character> getCharactersByName(String name){
+        return characterRepository.findByName(name);
+    }
+
+    public List<Character> getCharactersByUniverse(String universe){
+        return characterRepository.findByUniverse(universe);
+    }
 }
